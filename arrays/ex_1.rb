fe23e1e3ae7677be56a3e4c666543b1bd98f18dd
ron <ron.gregory@pacbell.net>
2015-04-28 05:12:41 +0000

    arrays

---
+++ arrays/ex_1.rb
@@ -0,0 +1,9 @@
+#ex_1.rb
+
+arr = [1, 3, 5, 7, 9, 11]
+
+number = 3
+
+if(arr.include?(3))
+  puts "Number was included in the array"
+end
---
+++ arrays/ex_7.rb
@@ -0,0 +1,9 @@
+#ex_7.rb
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+new_arr =[]
+
+arr.each{ |val| new_arr.push(val+2)}
+
+p arr
+p new_arr
